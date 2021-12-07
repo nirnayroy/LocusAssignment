28a67e3e065636c78c36e085d952ca10bbb57bad
nirnayroy <nroy1012@gmail.com>
2021-12-07 14:21:12 +0530

    local with data

--- DLA.py
+++ DLA.py
@@ -56,10 +56,12 @@ class Matrix:
 
         nParticles: no. of particles to aggregate
         '''
+        print('Start!!!')
         for i in range(nParticles):
             particle = self.spawnParticle(pos=self.posAtEdges(), stickiness = stickiness)
             self.walkParticle(particle)
-            print(i)
+            print('particle no.', i)
+        print('End')
 
     def posAtEdges(self):
         # get coordinates of the edge
@@ -152,7 +154,7 @@ class Matrix:
 
 
 class Simulator:
-    def __init__(self, M=251, nParticles=1500, stickiness = 1):
+    def __init__(self, M=51, nParticles=15, stickiness = 1):
         '''
         Initialize a simulator class
 
@@ -171,7 +173,7 @@ class Simulator:
         matrix = Matrix(self.M)
         matrix.seedMatrix()
         matrix.aggregate(self.nParticles, self.stickiness)
-        self.return_numpy(matrix)
+        return matrix.array
 
     def showImage(self, matrix: Matrix):
         '''
@@ -193,18 +195,9 @@ class Simulator:
         '''
         filename = f'M_{matrix.M}_NP_{self.nParticles}_ST_{self.stickiness}'
         np.save(filename, matrix.array)
-    
-    def return_numpy(self, matrix):
-        '''
-        saves a numpy array in the form of an .npy file
-
-        input:
-            matrix: A Matrix instance
-        '''
-        return matrix.array
 
 def main():
-    simulation = Simulator(M=100, nParticles=2000, stickiness = 1)
+    simulation = Simulator(M=251, nParticles=15000, stickiness = 1)
     simulation.run()
 
 if __name__ == '__main__':
